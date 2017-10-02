94592fcfafca7d0f40184c2e242e831716ab864a
YousifHmada <1401724@eng.asu.edu.eg>
2017-10-02 15:24:05 +0200

    =adding authentication

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 const _ = require('lodash');
+const {authenticate} = require('./middlewares/authenticate');
 
 var port = process.env.PORT || 3000;
 
@@ -12,10 +13,35 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req, res)=>{
+
+
+app.get('/users/info', authenticate, (req, res)=>{
+	res.send(req.user);
+});
+
+app.post('/users/signup',(req, res)=>{
+
+	var body = _.pick(req.body, ['email','password']);
+
+	var user = new User(body);
+
+	user.save()
+	.then(()=>{
+		return user.generateAuthToken();
+	})
+	.then((token)=>{
+		res.header('x-auth', token).status(200).send(user);
+	})
+	.catch((e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.post('/todos',authenticate ,(req, res)=>{
 
 	var todo = new Todo({
 		text: req.body.text,
+		_creator: req.user._id
 	});
 
 	todo.save()
@@ -27,9 +53,54 @@ app.post('/todos',(req, res)=>{
 	});
 });
 
-app.get('/todos',(req, res)=>{
+app.post('/users/login', (req, res)=>{
+	var body =  _.pick(req.body,['email','password']);
+	User.findByCredentials(body.email,body.password)
+		.then((user)=>{
+			return user.generateAuthToken().then((token)=>{
+				res.header('x-auth', token).status(200).send(user);
+			});
+		})
+		.catch((e)=>{
+			res.status(401).send({
+				error:'email and password don\'t match'
+			});
+		});
+});
 
-	Todo.find({})
+app.delete('/users/logout',authenticate , (req, res)=>{
+	req.user.removeToken(req.token)
+		.then((user)=>{
+			res.status(200).send(user);
+		})
+		.catch((e)=>{
+			res.status(400).send(e);
+		});
+});
+
+// app.get('/del',(req, res)=>{
+// 	User.remove({})
+// 	.then((users)=>{
+// 		res.status(200).send({users});
+// 	})
+// 	.catch((e)=>{
+// 		res.status(400).send(e);
+// 	});
+// });
+
+// app.get('/all',(req, res)=>{
+// 	User.find({})
+// 	.then((users)=>{
+// 		res.status(200).send({users});
+// 	})
+// 	.catch((e)=>{
+// 		res.status(400).send(e);
+// 	});
+// });
+
+app.get('/todos',authenticate ,(req, res)=>{
+
+	Todo.find({_creator:req.user._id})
 	.then((todos)=>{
 		res.status(200).send({todos});
 	})
@@ -38,14 +109,14 @@ app.get('/todos',(req, res)=>{
 	});
 });
 
-app.get('/todos/:id', (req, res)=>{
+app.get('/todos/:id',authenticate , (req, res)=>{
 	
 	if(!ObjectID.isValid(req.params.id)){
 		res.status(400).send({
 			error : 'please enter a valid id'
 		});
 	}
-	Todo.findById(req.params.id)
+	Todo.findOne({_id:req.params.id,_creator:req.user._id})
 		.then((todo)=>{
 			if(!todo){
 				res.status(400).send({
@@ -61,14 +132,14 @@ app.get('/todos/:id', (req, res)=>{
 
 });
 
-app.delete('/todos/:id', (req, res)=>{
+app.delete('/todos/:id',authenticate , (req, res)=>{
 
 	if(!ObjectID.isValid(req.params.id)){
 		res.status(400).send({
 			error : "please enter a valid id"
 		});
 	}
-	Todo.findByIdAndRemove(req.params.id)
+	Todo.findOneAndRemove({_id:req.params.id,_creator:req.user._id})
 		.then((todo)=>{
 			if(!todo){
 				res.status(400).send({
@@ -85,7 +156,7 @@ app.delete('/todos/:id', (req, res)=>{
 
 });
 
-app.patch('/todos/:id', (req, res)=>{
+app.patch('/todos/:id',authenticate , (req, res)=>{
 
 	if(!ObjectID.isValid(req.params.id)){
 		res.status(400).send({
@@ -103,7 +174,7 @@ app.patch('/todos/:id', (req, res)=>{
 	}
 
 
-	Todo.findByIdAndUpdate(req.params.id,{$set:body},{new:true})
+	Todo.findOneAndUpdate({_id:req.params.id,_creator:req.user._id},{$set:body},{new:true})
 		.then((todo)=>{
 			if(!todo){
 				res.status(400).send({
